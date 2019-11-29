037722b3917c8bc04aa04109905112d2d41a2c4e
deformartin <deformartin@gmail.com>
2019-11-29 15:48:41 +0100

    Updates

---
+++ precache-manifest.311d5cbaec5d06f2b4bf85cec53975d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9a7ade23a154318337d698f772a864",
+    "url": "/reactwork/index.html"
+  },
+  {
+    "revision": "984de392edfe0962f941",
+    "url": "/reactwork/static/css/main.374cedc8.chunk.css"
+  },
+  {
+    "revision": "6a0fc93339b1eea483c2",
+    "url": "/reactwork/static/js/2.3f4e9b4b.chunk.js"
+  },
+  {
+    "revision": "984de392edfe0962f941",
+    "url": "/reactwork/static/js/main.2960e66b.chunk.js"
+  },
+  {
+    "revision": "7c78e06b58f7a4d8ebbf",
+    "url": "/reactwork/static/js/runtime-main.0438a6fe.js"
+  }
+]);
